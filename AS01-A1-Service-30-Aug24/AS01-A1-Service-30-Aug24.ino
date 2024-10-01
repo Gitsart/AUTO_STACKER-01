fa77c2d6c90426cb2eacc997ee4f2c881994ad14
Gitsart <142268471+Gitsart@users.noreply.github.com>
2024-10-01 19:05:03 +0530

    Update AS01-A1-Service-01-OCT-24.ino

--- AS01-A1-Service-30-Aug24/AS01-A1-Service-30-Aug24.ino
+++ AS01-A1-Service-30-Aug24/AS01-A1-Service-30-Aug24.ino
@@ -1,9 +1,9 @@
 #include<MobaTools.h>
-const byte stepPin =18;
-const byte dirPin =17;
+const byte stepPin = 18;
+const byte dirPin = 17;
 
 #define limitPin A11
-const byte senPin= A10;
+const byte senPin = A10;
 #define startPin 14
 #define collectPin 9
 
@@ -11,16 +11,16 @@ const int stepsperRev = 300;
 
 
 long  nextPos;
-const long targetPos =3400;
+const long targetPos = 3400;
 
 int count = 0;
-int limit_count =0;
+int limit_count = 0;
 
-bool limswitch =0;
+bool limswitch = 0;
 
-int hOme =0;
-int pos =0;
-int sTart =0;
+int hOme = 0;
+int pos = 0;
+int sTart = 0;
 int cloth = 0;
 
 
@@ -36,89 +36,113 @@ bool clothDetected;
 
 void setup() {
   Serial.begin(9600);
-  stepper1.attach(stepPin,dirPin);
+  stepper1.attach(stepPin, dirPin);
   stepper1.setRampLen(10);
-  
+
   stepperRunning = false;
   movingForward = true;
   clothDetected = false;
-  
+
 
   pinMode(limitPin, INPUT_PULLUP);
   pinMode(startPin, INPUT_PULLUP);
   pinMode(senPin, INPUT_PULLUP);
-  pinMode(collectPin,INPUT_PULLUP);
+  pinMode(collectPin, INPUT_PULLUP);
   homing();
   set_pos();
-  
+
 }
 
 void loop() {
   int c = digitalRead(collectPin);
-  
-  
-    while(digitalRead(startPin)==LOW)
-    {sTart =1;}
 
-    
-    if((hOme ==2)&& (limit_count>1)&&(count ==0)&&(pos=1)&& (sTart ==1))
+
+  while (digitalRead(startPin) == LOW)
+  {
+    sTart = 1;
+  }
+
+
+  if ((hOme == 2) && (limit_count > 1) && (count == 0) && (pos = 1) && (sTart == 1))
   {
     int sen = digitalRead(senPin);
     bool sensorFound = false;
 
-    if(sen ==0 || sen==1)
+    if (sen == 0 || sen == 1)
     {
       sensorFound = true;
     }
-    
-    if(sen==1){
-      clothDetected =false;
+
+    if (sen == 1) {
+      clothDetected = false;
       motor();
-      Serial.print("CLOTHCOUNT:");Serial.println(cloth_count);
-  
-      }
+      Serial.print("CLOTHCOUNT:"); Serial.println(cloth_count);
+
     }
-    if(c==1) {
-      collect();
-      homing();
-      set_pos();
-    }
+  }
+  if (c == 1) {
+    collect();
+    homing();
+    set_pos();
+  }
 }
 
 void homing() {
   stepper1.setSpeed(1500);
-  while(digitalRead(limitPin) != HIGH && limit_count==0) { stepper1.rotate(1);Serial.print(limswitch);Serial.println("Finding END");}
-
-  if(digitalRead(limitPin) == HIGH && limit_count ==0) {stepper1.stop();Serial.println("END POSITION");limit_count+=1;limswitch=HIGH;hOme =1;}
-  stepper1.setZero();
-  delay(2000);
-  stepper1.setSpeed(200);stepper1.moveTo(-400);
-  limswitch =LOW;
-  delay(2000);
-  while(digitalRead(limitPin) != HIGH && limit_count==1) { stepper1.rotate(1);Serial.print(limswitch);Serial.println("Finding END2");}
-  if(digitalRead(limitPin) == HIGH && limit_count ==1) {stepper1.stop();Serial.println("END POSITION2");limit_count+=1;limswitch=HIGH;hOme =2;}
-  Serial.print("limit_count:");Serial.println(limit_count);
-  Serial.print("limswitch:");Serial.println(limswitch);
-  Serial.print("hOme:");Serial.println(hOme);
-  Serial.print("POS:");Serial.println(pos);
-  stepper1.setZero();
-  delay(2000);
+  while (digitalRead(limitPin) != HIGH && limit_count == 0) {
+    stepper1.rotate(1);
+    Serial.print(limswitch);
+    Serial.println("Finding END");
   }
 
+  if (digitalRead(limitPin) == HIGH && limit_count == 0) {
+    stepper1.stop();
+    Serial.println("END POSITION");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 1;
+  }
+  stepper1.setZero();
+  delay(2000);
+  stepper1.setSpeed(200); stepper1.moveTo(-400);
+  limswitch = LOW;
+  delay(2000);
+  while (digitalRead(limitPin) != HIGH && limit_count == 1) {
+    stepper1.rotate(1);
+    Serial.print(limswitch);
+    Serial.println("Finding END2");
+  }
+  if (digitalRead(limitPin) == HIGH && limit_count == 1) {
+    stepper1.stop();
+    Serial.println("END POSITION2");
+    limit_count += 1;
+    limswitch = HIGH;
+    hOme = 2;
+  }
+  Serial.print("limit_count:"); Serial.println(limit_count);
+  Serial.print("limswitch:"); Serial.println(limswitch);
+  Serial.print("hOme:"); Serial.println(hOme);
+  Serial.print("POS:"); Serial.println(pos);
+  stepper1.setZero();
+  delay(2000);
+}
+
 void set_pos() {
-  
-  while(hOme ==2 && limit_count >1 && limswitch ==HIGH && pos ==0) {
-    
+
+  while (hOme == 2 && limit_count > 1 && limswitch == HIGH && pos == 0) {
+
     delay(1000);
     stepper1.setSpeed(2000);
-    stepper1.moveTo(-4300);
+    stepper1.moveTo(-4100);
 
-    while(stepper1.moving()){Serial.println("MOVING to Pos1");}
-
-    pos =1;
-    Serial.println("StartPositionSet, READY TO USE");
+    while (stepper1.moving()) {
+      Serial.println("MOVING to Pos1");
     }
-    Serial.print("POS:");Serial.println(pos);
+
+    pos = 1;
+    Serial.println("StartPositionSet, READY TO USE");
+  }
+  Serial.print("POS:"); Serial.println(pos);
 }
 
 void motor() {
@@ -152,14 +176,4 @@ void motor() {
 
     stepperRunning = false; // Reset the flag when the complete oscillation is done
   }
-}
-void collect() {
-  stepper1.setSpeed(1500);
-  stepper1.moveTo(targetPos);
-  stepperPause.setTime(5000);
-  cloth_count=0;
-  sTart = 0;
-  limit_count = 0;
-  hOme=0;
-  pos=0;
 }
